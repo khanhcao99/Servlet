ecebf047b13d91cab7cb416003f16c2bb6be43c4
khanhcao99 <khanhboy1324@gmail.com>
2022-07-21 15:02:29 +0700

    simple_login

---
+++ simple_login/src/main/java/com/example/simple_login/HelloServlet.java
@@ -0,0 +1,28 @@
+package com.example.simple_login;
+
+import java.io.*;
+import javax.servlet.ServletException;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+
+@WebServlet(name = "helloServlet", value = "/login")
+public class HelloServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String username = req.getParameter("username");
+        String password = req.getParameter("password");
+
+        PrintWriter writer = resp.getWriter();
+        writer.println("<html>");
+        if ("admin".equals(username) && "admin".equals(password)){
+            writer.println("<h1>Welcome " + username + " to website</h1>");
+        } else {
+            writer.println("<h1>Login Error</h1>");
+        }
+        writer.println("</html>");
+    }
+
+    public void destroy() {
+    }
+}
